b81d155a58899b5d983a244ae728239abe8277ba
Aashu1308 <aashu.sekhar@gmail.com>
2024-09-07 13:18:56 +0530

    added location features

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
 
 app = Flask(__name__)
 
@@ -8,17 +10,28 @@ with open('weights.pkl', 'rb') as mfile:
     model = pickle.load(mfile)
 
 
-@app.route('/predict', methods=['POST'])
+def calculate_distance(origin, destination):
+    loc = Nominatim(user_agent="GetLoc")
+    origin = loc.geocode(origin)
+    o_coords = (origin.latitude, origin.longitude)
+    destination = loc.geocode(destination)
+    d_coords = (destination.latitude, destination.longitude)
+    return round((geodesic(o_coords, d_coords).km), 3)
+
+
+@app.route('/api/predict', methods=['POST'])
 def predict():
     data = request.json
     print("Received data:", data)
-    weight = data['weight']
-    distance = data['distance']
-    loading_d = data['loading_meters']
+    weight = float(data['weight'])
+    origin = data['origin']
+    destination = data['destination']
+    distance = calculate_distance(origin, destination)
+    loading_d = 0.000195  # data['loading_meters']
     features = np.array([[weight, loading_d, distance]])
     results = model.predict(features)
     return jsonify({'predictions': float(results[0])})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
