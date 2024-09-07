ad71877392a4b93a0b8b10363e62d8c267375c8f
Aashu1308 <aashu.sekhar@gmail.com>
2024-09-07 13:34:50 +0530

    Error handling

--- app.py
+++ app.py
@@ -12,25 +12,36 @@ with open('weights.pkl', 'rb') as mfile:
 
 def calculate_distance(origin, destination):
     loc = Nominatim(user_agent="GetLoc")
-    origin = loc.geocode(origin)
-    o_coords = (origin.latitude, origin.longitude)
-    destination = loc.geocode(destination)
-    d_coords = (destination.latitude, destination.longitude)
+    origin_location = loc.geocode(origin)
+    if origin_location is None:
+        raise AttributeError(f"Origin '{origin}' could not be found.")
+    destination_location = loc.geocode(destination)
+    if destination_location is None:
+        raise AttributeError(f"Destination '{destination}' could not be found.")
+    o_coords = (origin_location.latitude, origin_location.longitude)
+    d_coords = (destination_location.latitude, destination_location.longitude)
     return round((geodesic(o_coords, d_coords).km), 3)
+
+
+@app.errorhandler(400)
+def handle_400_error(error):
+    return jsonify({'error': 'Bad request', 'message': str(error)}), 400
 
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    data = request.json
-    print("Received data:", data)
-    weight = float(data['weight'])
-    origin = data['origin']
-    destination = data['destination']
-    distance = calculate_distance(origin, destination)
-    loading_d = 0.000195  # data['loading_meters']
-    features = np.array([[weight, loading_d, distance]])
-    results = model.predict(features)
-    return jsonify({'predictions': float(results[0])})
+    try:
+        data = request.get_json()
+        weight = float(data['weight'])
+        origin = data['origin']
+        destination = data['destination']
+        distance = calculate_distance(origin, destination)
+        loading_d = 0.000195  # data['loading_meters']
+        features = np.array([[weight, loading_d, distance]])
+        results = model.predict(features)
+        return jsonify({'predictions': float(results[0])})
+    except (ValueError, AttributeError) as e:
+        return handle_400_error(e)
 
 
 if __name__ == '__main__':
